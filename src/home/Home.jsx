b3702bbbef7c0cb34ed25ee198e114fae468f708
francyelle349 <francyelle5849@gmail.com>
2025-06-18 17:22:06 -0400

    add link

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
           <p className="pt-2 pl-8" id="description">I design and develop systems and Websites</p>
         
           <div className="pt-8 pl-8 flex items-center justify-start">
-            <a href={`/cv.pdf`} 
+            <a href={`https://drive.google.com/file/d/1TudpGwGwfXRSCL5TbRFXasEPW4r3p1xL/view?usp=sharing`} 
                target="_blank">
               <p className="glass-button">Hire me | +</p>
             </a>
