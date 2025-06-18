62ad39ae5b449e3d6c65dfe15f2aa0402797af56
francyelle349 <francyelle5849@gmail.com>
2025-06-18 16:44:54 -0400

    updates

--- src/home/redesSociais/redes.jsx
+++ src/home/redesSociais/redes.jsx
@@ -1,35 +1,34 @@
 import './redes.css'
+import womanImage from '../resources/woman2.svg';
 
 function Redes(){
-    return(<>
+    return(
     <div className='item'>
-      <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"></link>
-
+      {/* Move o link do CSS para o <head> do index.html para melhor performance */}
+      
       <div className='subItem'>
-
-
-        
-
-          
-          <h3>You Can Reach Out </h3>
-        <div className='socialMedia'>
-       
-        <a href="https://www.linkedin.com/in/francyelle-souza-245968350/"> <i className="uil uil-linkedin" id="link"></i> </a>
-        <a href="https://github.com/francyelle349"> <i className="uil uil-github" id="git" ></i> </a>
-
-        
-        </div>
-
+          <h3>You Can Reach Out</h3>
+          <div className='socialMedia'>
+            <a href="https://www.linkedin.com/in/francyelle-souza-245968350/" 
+               target="_blank" 
+               rel="noopener noreferrer"
+               aria-label="LinkedIn"> 
+              <i className="uil uil-linkedin" id="link"></i> 
+            </a>
+            <a href="https://github.com/francyelle349" 
+               target="_blank" 
+               rel="noopener noreferrer"
+               aria-label="GitHub"> 
+              <i className="uil uil-github" id="git"></i> 
+            </a>
+          </div>
       </div>
 
       <div className='images'>
-        <img src="/portfolio_fran/src/home/resources/woman2.svg" alt="" />
+        <img src={`${womanImage}`} alt="Illustration of a woman" />
       </div>
-
-        
-
     </div>
-    </>);
+    );
 }
 
 export default Redes;
--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   css: {
     postcss: './postcss.config.js',
   },
-   base: '/portfolio_fran/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio_fran/' : '/',
   build: {
     outDir: 'dist'
   }
-}
-
-
-)
+})
